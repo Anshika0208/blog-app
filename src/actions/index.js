20e734f07fc1e030db12e0689980004acb2bda52
Anshika0208 <jainanshika2416@gmail.com>
2021-09-10 23:40:38 +0530

    Added code for displaying commennt list

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,11 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => {
   return async (dispatch) => {
-    const promise = await jsonPlaceholder.get("/posts");
+    const response = await jsonPlaceholder.get("/posts");
 
     dispatch({
       type: "FETCH_POSTS",
-      payload: promise,
+      payload: response.data,
     });
   };
 };
